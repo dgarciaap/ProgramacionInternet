f8f4d670eeeaecdb42f23d1fe1938c7efb51b5ae
dgarciaap <dgarcia24@ucol.mx>
2019-03-25 08:28:54 -0600

    Refactor

--- app.js
+++ app.js
@@ -1,46 +1,31 @@
+'use strict';
+
 //Objeto hereda de Events
 var EventEmitter = require('events');
 var util = require('util');
 
-function Greetr() {
-    EventEmitter.call(this); //Super constructor (passed by reference)
-    this.greeting = 'Hello';
+class Greeter extends EventEmitter{ //Refactor
+    constructor(firstname, lastname) {
+        super();//Al agregar el extends para herencia
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.greeting = 'Someone greeted ';
+    }
+
+    greet(data) {
+        console.log('Hello, ' + this.firstname + ' ' + this.lastname);
+        console.log(`${ this.greeting }: ${ data }`);
+        this.emit('greet', data);
+        //console.log('Hello, ' + this.firstname + ' ' + this.lastname);
+    }
 }
+var greeter1 = new Greeter('John', 'Doe');
+greeter1.greet('John');
+//util.inherits(Greeter, EventEmitter);//Hereda en forma incompleta (usar super constructor)
 
-util.inherits(Greetr, EventEmitter);//Hereda en forma incompleta (usar super constructor)
+/*var john = new Greeter('John','Doe');
+john.greet();
 
-Greetr.prototype.greet = function(data) {
-    console.log(this.greeting + ': ' + data);//Primero imprime Hello world y luego emite greet que puede ser definido
-    this.emit('greet', data);
-}
-
-var greeter1 = new Greetr();
-
-greeter1.on('greet', function(data) { //on está definido en 'events'
-    console.log('Someone greeted' + ': ' + data);//llama primero a la cadena de prototipos, that's why imprime Hello world y luego emite greet
-});
-
-greeter1.greet('Tony');
-
-/*Ejemplo ll*/
-
-var util = require('util');
-
-function Person() {
-    this.firstname = 'John';
-    this.lastname = 'Doe';
-}
-
-Person.prototype.greet = function() {
-    console.log('Hello ' + this.firstname + ' ' + this.lastname);
-}
-
-function Policeman() {
-    Person.call(this); //Super constructor
-    this.badgenumber = '1234';
-}
-
-util.inherits(Policeman, Person);//incompleto (undefined)
-var officer = new Policeman();
-officer.greet();
+var jane = new Greeter('John','Doe');
+jane.greet();*/
 
