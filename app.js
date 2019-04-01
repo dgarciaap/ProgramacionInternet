e1dc1164413f9cd63a55d67c0cd62c15f684a230
dgarciaap <dgarcia24@ucol.mx>
2019-04-01 08:12:18 -0600

    Servidor http

--- app.js
+++ app.js
@@ -1,31 +1,9 @@
-'use strict';
+var http = require('http');
 
-//Objeto hereda de Events
-var EventEmitter = require('events');
-var util = require('util');
+http.createServer(function(req, res) { //request and response
+    
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
 
-class Greeter extends EventEmitter{ //Refactor
-    constructor(firstname, lastname) {
-        super();//Al agregar el extends para herencia
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.greeting = 'Someone greeted ';
-    }
-
-    greet(data) {
-        console.log('Hello, ' + this.firstname + ' ' + this.lastname);
-        console.log(`${ this.greeting }: ${ data }`);
-        this.emit('greet', data);
-        //console.log('Hello, ' + this.firstname + ' ' + this.lastname);
-    }
-}
-var greeter1 = new Greeter('John', 'Doe');
-greeter1.greet('John');
-//util.inherits(Greeter, EventEmitter);//Hereda en forma incompleta (usar super constructor)
-
-/*var john = new Greeter('John','Doe');
-john.greet();
-
-var jane = new Greeter('John','Doe');
-jane.greet();*/
+}).listen(1337, '127.0.0.1');//port
 
