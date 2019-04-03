dcd93b7087776a7a24cb018559edd0ea62245fbf
dgarciaap <dgarcia24@ucol.mx>
2019-04-03 07:49:51 -0600

    Esqueleto html

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function(req, res) { //request and response
     
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var html = fs.readFileSync(__dirname + '/index.html','utf8');//utf8 formato en que me regresa los datos (string)
+    var message = "Hello world...";
+    html = html.replace('{Message}', message);//Busca string {Message} y reemplaza
+    res.end(html);
 
 }).listen(1337, '127.0.0.1');//port
 
+//template: text designed to be the basis for final text {Message} at index.html
