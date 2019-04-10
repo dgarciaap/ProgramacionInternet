78b02bec24f1b9e96681bef466a06ce5c1b81868
dgarciaap <dgarcia24@ucol.mx>
2019-04-10 08:18:41 -0500

    "Express"

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var app = express();
 
-http.createServer(function(req, res) { //request and response
-    if(req.url === '/'){
-        fs.createReadStream(__dirname + '/index.html').pipe(res);
-    }
-    
-    else if(req.url === '/api'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        var obj = {
-        firstname: 'John',
-        lastname: 'Doe'
-        };
-        res.end(JSON.stringify(obj));//JSON key value pairs
-    }
-    else {
-        res.writeHead(404);
-        res.end();
-    }
+//Environmente variables: global variables specific to the environment (server) our code is living in
+var port = process.env.PORT || 3000; //asigna el 3000 con || sólo si está disponible
 
-}).listen(1337, '127.0.0.1');//port
+//http method: specifies the type of action the request wishes to make (verbs: GET, POST, DELETE)
+app.get('/', function(req, res) {
+    res.send('<html><head><body><h1>Hello world!</h1></body></head></html>');
+});
 
-//serialize: translating an object into a format that can be stored or transferred
-//routing: mapping http request to content
+//agregar ruta de parametros
+app.get('/person/:id', function(req, res) {
+    res.send('<html><head><body><h1>Person: ' + req.params.id + '</h1></body></head></html>');
+});
+
+//Express ya tiene json incluido. Pedir objeto
+app.get('/api', function(req, res) {
+    res.json({firstname: 'John', lastname: 'Doe'});
+});
+
+app.listen(port); //Create Server, http port
