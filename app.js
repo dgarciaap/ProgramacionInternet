b8dc871b70591face04811a13fb163891dae1bbd
cesar magallon <cesar9mv@gmail.com>
2019-05-13 09:49:38 -0500

    comentarios

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 var request = require('request');
 
 request("https://us1.locationiq.com/v1/search.php?key=ea3e3eae4b14c1&q=Colima&format=json", function (error, response, body) {
+  //Abre promesa con resolve y reject. Resolve regresa data y reject error
     p1 = new Promise(
     function(resolve, reject) {
+      //Si recibe data 
       if(body) {
         let info = JSON.parse(body);
         let lati = info[0].lat;
         let lngi = info[0].lon;
+        //regresas data en array
         resolve([lati,lngi]);
       } else {
+        //Regresas error
         reject(err);
       }
-    }).then(function(coordenadas) {
+    }).then(function(coordenadas) { //Entonces recibe array y lo imprime
       console.log('Lat: ' + coordenadas[0] + '\n' + 'long: ' + coordenadas[1]);
+      //Hacer request en otro nivel
       request(`https://api.darksky.net/forecast/366bf5ba25d241ff04fb0c781f5c1c92/${coordenadas[0]},${coordenadas[1]}`, function (error, response, body) {
-          p2 = new Promise(
+          p2 = new Promise( //Nueva promesa para obtener weather
           function(resolve, reject) {
             if(body) {
               let info = JSON.parse(body);
@@ -24,13 +29,13 @@ request("https://us1.locationiq.com/v1/search.php?key=ea3e3eae4b14c1&q=Colima&fo
               reject(err);
             }
           }
-        ).then(function(weather) {
+        ).then(function(weather) { //Recibe weather y lo imprime
           console.log(`La temperatura es de ${weather} F°`);
-        }).catch(function(err) {
+        }).catch(function(err) { //Catchear error
           console.log(err);
         })
     });
-    var options = { method: 'GET',
+    var options = { method: 'GET', //Hacer request al mismo nivel del segundo (no necesitamos coordenadas eda)
     url: 'https://api.openuv.io/api/v1/uv',
     qs: { lat: `${coordenadas[0]}`, lng: `${coordenadas[1]}`},
     headers: 
@@ -38,16 +43,16 @@ request("https://us1.locationiq.com/v1/search.php?key=ea3e3eae4b14c1&q=Colima&fo
         'x-access-token': 'f9936e83b61fb156275253e5f276fc50' } }
 
         request(options, function (error, response, body) {
-            p3 = new Promise(function (resolve, reject) {
+            p3 = new Promise(function (resolve, reject) { //Definir 3ra promesa
               if(body) {
                 let uv = JSON.parse(body);
                 resolve(uv);
               } else {
                 reject(err);
               }
-            }).then(function(uv) {
+            }).then(function(uv) { //Recibe uv y lo imprime
               console.log(uv);
-            }).catch(function(err) {
+            }).catch(function(err) { //Finalmente catchea cualquier error
               console.log(err);
             }) 
           });
